6d15387931f842160c89b3921f160ca63765f275
ELEXILON <elexilon@gmail.com>
2018-02-15 16:26:22 +0100

    add navbar

---
+++ src/components/ui/Navigation.js
@@ -0,0 +1,74 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+//import signOut from '../../actions/user/sign-out'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from 'reactstrap'
+
+const TITLE = 'Hunger Games'
+
+class Navigation extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  static propTypes = {
+    signedIn: PropTypes.bool.isRequired,
+    push: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }
+
+  signOut = (event) => {
+    event.preventDefault()
+    this.props.signOut()
+  }
+
+  signUp = () => {
+    this.props.push('/sign-up')
+  }
+
+  goHome = () => {
+    this.props.push('/')
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    const { signedIn } = this.props
+    return (
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">{TITLE}</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+              {signedIn ?
+                <Button color="link" onClick={this.signOut.bind(this)} >signOut</Button> :
+                <Button color="link" onClick={this.signUp} >signUp</Button> }
+              </NavItem>
+              <NavItem>
+                <Button color="link" onClick={this.signUp} >signIn</Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: (!!currentUser && !!currentUser._id)
+})
+
+export default connect(mapStateToProps, { push })(Navigation)
